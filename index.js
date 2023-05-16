ba97df332b734466b34608e4402ed38d16c810fb
unknown <aboelyazid11@gmail.com>
2023-05-16 17:39:25 +0300

    Upload The Design

---
+++ index.js
@@ -0,0 +1,13 @@
+// const myIcon = document.querySelector(".icon");
+// const myList = document.querySelector("ul");
+// const container = document.querySelector(".container");
+
+// myIcon.addEventListener('click', function() {
+//   myList.classList.toggle('hidden');
+// });
+
+// container.addEventListener('click', function(event) {
+//   if (container.contains(event.target)) {
+//     myList.classList.add('hidden');
+//   }
+// });
